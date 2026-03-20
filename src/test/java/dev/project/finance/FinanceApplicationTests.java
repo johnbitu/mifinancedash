a0d23011718e8da824e058f5ccf7bdd720268588
João Victor Bitu <bitujoaovictor@gmail.com>
2026-03-20 15:22:30 -0300

    Fix Maven wrapper and Boot 4 test setup to run unit tests

--- src/test/java/dev/project/finance/FinanceApplicationTests.java
+++ src/test/java/dev/project/finance/FinanceApplicationTests.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
@@ -27,8 +27,7 @@ class FinanceApplicationTests {
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     void registerSempreCriaUsuarioComRoleUsuario() throws Exception {
